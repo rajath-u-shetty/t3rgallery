fd8fadd427e9c856ac493ad6a88787a299306361
Rajath U Shetty <rajathushetty56@gmail.com>
2024-07-17 00:50:29 +0530

    Using auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,37 @@
 import Link from "next/link";
 import Image from "next/image";
 import { db } from "~/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+export default function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sign in</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
+    </main>
+  );
+}
+
+export async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.createdAt),
   });
 
-  console.log(images);
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4 ">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "_" + index} className="w-48">
-            <img src={image.url} alt="image" width={300} height={300} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
-    </main>
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + "_" + index} className="w-48">
+          <Image src={image.url} alt={image.name} width={300} height={300} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
   );
 }
+
